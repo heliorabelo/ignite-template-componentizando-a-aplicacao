adcb08714675178f4939dec826b7ed50a48911d4
Helio Souza <helio.souza@oi.net.br>
2021-11-23 19:26:13 -0300

    Desafio finalizado

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,55 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+import { Content } from "./Content";
+
+interface GenreResponseProps {
+  genres: Array<{
+    id: number;
+    name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+    title: string;
+  }>;
+  selectedGenreId: number;
+  handleClickButton: (id: number) => void;
+  selectedGenre: {
+    id: number;
+    name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+    title: string;
+  };
+
+  movies: Array<{
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }>;
+}
+
+export function SideBar(props: GenreResponseProps) {
+  // Complete
+
+  return (
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      <nav className="sidebar">
+        <span>
+          Watch<p>Me</p>
+        </span>
+
+        <div className="buttons-container">
+          {props.genres.map((genre) => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => props.handleClickButton(genre.id)}
+              selected={props.selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+      </nav>
+      <Content selectedGenre={props.selectedGenre} movies={props.movies} />
+    </div>
+  );
 }
